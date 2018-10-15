15bb7fbe915c7a9b7368fcdadceecb8aceb6df9c
和田真理菜 <marina.wada@topgate.co.jp>
2018-10-15 19:06:44 +0900

    キャラ一覧の作成

---
+++ pages/js/character_list_variables.js
@@ -0,0 +1,199 @@
+const first = [
+    {
+        name: 'レオンニール・フレヴァ',
+        // img: '../',
+        url: '1_leoniel.html',
+        // profile: '炎',
+    },
+    /* ヒロイン
+    {
+        name: '',
+        img: '../',
+        url: '1_.html',
+    }, */
+    {
+        name: 'ビースト',
+        // img: '../',
+        // url: '1_beast.html',
+    }, {
+        name: 'アルテミア',
+        // img: '../',
+        // url: '1_artemia.html',
+    }, {
+        name: 'メリッサ',
+        // img: '../',
+        // url: '1_melissa.html',
+    },
+];
+
+const second = [
+    {
+        name: 'キルデバート・アム・トリストラム',
+        // img: '../',
+        url: '2_kirdebert.html',
+    }, {
+        name: 'ヴェデ',
+        // img: '../',
+        url: '2_verde.html',
+    }, {
+        name: 'リーレル・カリストロ',
+        // img: '../',
+        url: '2_rirell.html',
+    }, {
+        name: 'ヒュプノス・デアトッド',
+        // img: '../',
+        url: '2_hypnus.html',
+    },
+    /* 悪魔っこちゃん
+    {
+        name: '',
+        // img: '../',
+        url: '2_.html',
+    }, */
+    {
+        name: 'アレクシス・ラザフォード',
+        // img: '../',
+        url: '2_alexis.html',
+    }, {
+        name: 'クリストファー',
+        // img: '../',
+        // url: '2_christopher.html',
+    },
+];
+
+const second_another = [
+    {
+        name: 'ラインハルト',
+        // img: '../',
+        url: '2_reinhart.html',
+    }, {
+        name: 'ジークフリート',
+        // img: '../',
+        url: '2_siegfried.html',
+    }, {
+        name: 'ランスロット',
+        // img: '../',
+        url: '2_lancelot.html',
+    }, {
+        name: 'ガウェイン',
+        // img: '../',
+        url: '2_gawein.html',
+    },
+];
+
+const third = [
+    {
+        name: 'カイルス',
+        // img: './img/kairus_ico.png',
+        url: '3_kairus.html',
+    }, {
+        name: 'クレハ',
+        // img: './img/kureha_ico.png',
+        url: '3_kureha.html',
+    }, {
+        name: 'フェイン',
+        // img: './img/fein_ico.png',
+        url: '3_fein.html',
+    }, {
+        name: 'ロッド・デアトッド',
+        // img: './img/lod_ico.png',
+        url: '3_lod.html',
+    }, {
+        name: 'サーク・ヴァナント',
+        // img: './img/sarc_ico.png',
+        url: '3_sarc.html',
+    }, {
+        name: 'ティアナ・リーゼロッテ',
+        // img: './img/tiana_ico.png',
+        url: '3_reinhart.html',
+    }, {
+        name: 'ザキ・ガルダンヴァ',
+        // img: './img/zhaki_ico.png',
+        url: '3_zhaki.html',
+    }, {
+        name: 'エリス・ティロンド',
+        // img: './img/eris_ico.png',
+        url: '3_eris.html',
+    }, {
+        name: 'ミド・アルテクス',
+        // img: './img/mid_ico.png',
+        url: '3_mid.html',
+    }, {
+        name: 'カオル',
+        // img: './img/kaoru_ico.png',
+        // url: '3_kaoru.html',
+    },
+];
+
+const fourth = [
+    {
+        name: 'テラ・クルセイド',
+        // img: './img/tera_ico.png',
+        url: '4_tera.html',
+    }, {
+        name: 'ソティラス',
+        // img: './img/sotilas_ico.png',
+        url: '4_sotilas.html',
+    }, {
+        name: 'シュトライ・バロッツ',
+        // img: './img/streit_ico.png',
+        url: '4_streit.html',
+    }, {
+        name: 'リッター・リェカーヴ・R・オルコス',
+        // img: './img/ritter_ico.png',
+        url: '4_ritter.html',
+    }, {
+        name: 'シン',
+        // img: './img/shin_ico.png',
+        url: '4_shin.html',
+    }, {
+        name: 'フェアラート・トライゾン',
+        // img: './img/verrat_ico.png',
+        url: '4_verrat.html',
+    }, {
+        name: 'マルシュアース',
+        // img: './img/marthearth_ico.png',
+        // url: '4_marthearth.html',
+        profile: 'ゴーレム。もっとも機動力が高いプロトタイプ。'
+    }, {
+        name: 'クロック・オクロック',
+        // img: './img/oclock_ico.png',
+        // url: '4_oclock.html',
+        profile: 'ゴーレム。時計の形をしているが時間にはルーズ。'
+    }, {
+        name: 'ノーチラス',
+        // img: './img/nautilus_ico.png',
+        // url: '4_nautilus.html',
+        profile: 'ゴーレム。水は錆びるので苦手。'
+    }, {
+        name: 'エリザヴェート',
+        // img: './img/elisabeth_ico.png',
+        // url: '4_elisabeth.html',
+    }, {
+        name: 'バク・ジュッラーレ',
+        // img: './img/baku_ico.png',
+        url: '4_baku.html',
+    }, {
+        name: 'メアリー',
+        // img: './img/mary_ico.png',
+        url: '4_mary.html',
+    },
+];
+
+const fifth = [
+    {
+        name: 'バル・バロイ',
+        // img: './img/baru_ico.png',
+        // url: '5_.html',
+    },
+    {
+        name: 'アリア',
+        // img: './img/aria_ico.png',
+        // url: '5_aria.html',
+    },
+    {
+        name: 'エルデ・ラント',
+        // img: './img/erde_ico.png',
+        // url: '5_erde.html',
+    },
+];
